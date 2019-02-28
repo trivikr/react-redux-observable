82b23cce707bb295af6b5042e8c7e7fc222dec33
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-02-28 15:22:22 -0800

    Handler actions in reducer

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,5 +1,5 @@
 import { Reducer } from "redux";
-import { StoreState } from "./types";
+import { StoreState, StoreActionType } from "./types";
 
 const initialState: StoreState = {
   whiskies: [], // for this example we'll make an app that fetches and lists whiskies
@@ -9,6 +9,27 @@ const initialState: StoreState = {
 
 const rootReducer: Reducer<StoreState> = (state = initialState, action) => {
   switch (action.type) {
+    case StoreActionType.FETCH_WHISKIES:
+      return {
+        ...state,
+        // whenever we want to fetch the whiskies, set isLoading to true to show a spinner
+        isLoading: true,
+        error: ""
+      };
+    case StoreActionType.FETCH_WHISKIES_SUCCESS:
+      return {
+        whiskies: [...action.payload],
+        // whenever the fetching finishes, we stop showing the spinner and then show the data
+        isLoading: false,
+        error: ""
+      };
+    case StoreActionType.FETCH_WHISKIES_FAILURE:
+      return {
+        whiskies: [],
+        isLoading: false,
+        // same as FETCH_WHISKIES_SUCCESS, but instead of data we will show an error message
+        error: action.payload
+      };
     default:
       return state;
   }
